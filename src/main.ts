97b951310b48d6c429863c4ea1e410f12a57f6a9
Marko Stevanović <stevanovicmarko@gmail.com>
2022-05-01 19:11:46 +0200

    Fixed styling and added fullscreen toggle.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "./style.css";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
@@ -16,8 +17,27 @@ camera.lookAt(cube.position);
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setAnimationLoop(animation);
+
+
+renderer.domElement.className = "web-gl";
 document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+    if (!document.fullscreenElement) {
+        renderer.domElement.requestFullscreen().catch(err => console.error(err));
+    } else {
+        document.exitFullscreen().catch(err => console.error(err));
+    }
+});
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
