e7fefa473e8bf1b1cb44db4fa5e823fa6bf97be7
Marko Stevanović <stevanovicmarko@gmail.com>
2022-07-17 22:37:19 +0200

    Started working on raycaster demo. Added sphere intersection.

--- src/main.ts
+++ src/main.ts
@@ -5,85 +5,38 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
-const params = {
-    count: 100_000,
-    size: 0.01,
-    radius: 5,
-    galaxyArmsCount: 10,
-    galaxySpin: 1,
-    randomness: 0.2,
-    randomnessPower: 3,
-    insideColor: 0xff6030,
-    outsideColor: 0x1b3984
-} as const;
+const obj1 = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+obj1.position.x = -2;
 
-let geometry: THREE.BufferGeometry | undefined;
-let material: THREE.Material | undefined;
-let points: THREE.Points | undefined;
+const obj2 = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
 
-function generateGalaxy() {
+const obj3 = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+obj3.position.x = 2;
 
-    geometry?.dispose();
-    material?.dispose();
-    if (points) {
-        scene.remove(points);
-    }
+scene.add(obj1, obj2, obj3);
 
-    geometry = new THREE.BufferGeometry();
-    const positions = new Float32Array(params.count * 3);
-    const colors = new Float32Array(params.count * 3);
-    const colorsInside = new THREE.Color(params.insideColor);
-    const colorsOutside = new THREE.Color(params.outsideColor);
-
-    for (let i = 0; i < params.count; i++) {
-        const offset = i * 3;
-        const radius = Math.random() * params.radius;
-        const spinAngle = radius * params.galaxySpin;
-        const armAngle = (i % params.galaxyArmsCount) / params.galaxyArmsCount * Math.PI * 2;
-
-        const randomX = Math.pow(Math.random(), params.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * params.randomness;
-        const randomY = Math.pow(Math.random(),  params.randomnessPower) *(Math.random() < 0.5 ? 1 : -1) * params.randomness;
-        const randomZ = Math.pow(Math.random(), params.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * params.randomness;
-
-        positions[offset] = Math.cos(armAngle + spinAngle) * radius + randomX;
-        positions[offset + 1] = randomY;
-        positions[offset + 2] = Math.sin(armAngle + spinAngle) * radius + randomZ;
-
-        const mixedColor = colorsInside.clone();
-        mixedColor.lerp(colorsOutside, radius / params.radius);
-        colors[offset] = mixedColor.r;
-        colors[offset + 1] = mixedColor.g;
-        colors[offset + 2] = mixedColor.b;
-    }
-
-    console.log(colors);
-    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-
-    material = new THREE.PointsMaterial({
-        size: params.size,
-        sizeAttenuation: true,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending,
-        vertexColors: true
-    });
-
-    points = new THREE.Points(geometry, material);
-    scene.add(points);
-}
-
-generateGalaxy();
-
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 100);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 10);
 camera.position.z = 4;
-camera.position.y = 4;
+
+const raycaster = new THREE.Raycaster();
+const rayOrigin = new THREE.Vector3(-3, 0, 0);
+const rayDirection = new THREE.Vector3(10, 0, 0).normalize();
+raycaster.set(rayOrigin, rayDirection);
+
+
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
 renderer.setAnimationLoop(animation);
 
 
@@ -110,64 +63,24 @@ controls.enableDamping = true;
 
 const gui = new dat.GUI();
 gui.width = 400;
-gui.add(params, 'count')
-    .min(100)
-    .max(1_000_000)
-    .step(100)
-    .name("Stars count")
-    .onFinishChange(generateGalaxy);
-gui.add(params, 'size')
-    .min(0.001)
-    .max(0.1)
-    .step(0.001)
-    .name("Star size")
-    .onFinishChange(generateGalaxy);
 
-gui.add(params, 'radius')
-    .min(0.01)
-    .max(20)
-    .step(0.001)
-    .name("Galaxy Radius")
-    .onFinishChange(generateGalaxy);
-
-gui.add(params, 'galaxyArmsCount')
-    .min(2)
-    .max(20)
-    .step(1)
-    .name("Galaxy Arms Count")
-    .onFinishChange(generateGalaxy);
-
-gui.add(params, 'galaxySpin')
-    .min(-5)
-    .max(5)
-    .step(0.01)
-    .name("Galaxy Spin")
-    .onFinishChange(generateGalaxy);
-
-gui.add(params, 'randomness')
-    .min(0)
-    .max(2)
-    .step(0.01)
-    .name("Galaxy Randomness")
-    .onFinishChange(generateGalaxy);
-
-gui.add(params, 'randomnessPower')
-    .min(1)
-    .max(10)
-    .step(0.01)
-    .name("Galaxy Randomness Weight")
-    .onFinishChange(generateGalaxy);
-
-gui.addColor(params, 'insideColor')
-    .name("Inside Color")
-    .onFinishChange(generateGalaxy);
-
-gui.addColor(params, 'outsideColor')
-    .name("Outside Color")
-    .onFinishChange(generateGalaxy);
-
+const clock = new THREE.Clock();
+const objectsToTest = [obj1, obj2, obj3];
 function animation() {
+    const elapsedTime = clock.getElapsedTime();
+    const intersectedObjects = raycaster.intersectObjects(objectsToTest);
 
+    for (const obj of objectsToTest) {
+        obj.material.color.set(0xff0000);
+    }
+
+    for (const obj of intersectedObjects) {
+        const color = (obj.object as THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial>).material.color;
+        color.set(0x0000ff);
+    }
+    obj1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+    obj2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+    obj3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
     controls.update();
     renderer.render(scene, camera);
 }
